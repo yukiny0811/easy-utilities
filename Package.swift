76f731e9a5d8bb59771a20a7859ffadb192cb21a
yukiny0811 <yukiny0811@gmail.com>
2023-11-29 04:28:09 +0900

    initial

--- Package.swift
+++ Package.swift
@@ -5,19 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "EasyUtilities",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "EasyUtilities",
-            targets: ["EasyUtilities"]),
+            targets: ["EasyUtilities"]
+        ),
+    ], 
+    dependencies: [
+        .package(url: "git@github.com:yukiny0811/easy-string.git", from: "1.0.0"),
+        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", from: "4.1.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "EasyUtilities"),
-        .testTarget(
-            name: "EasyUtilitiesTests",
-            dependencies: ["EasyUtilities"]),
+            name: "EasyUtilities",
+            dependencies: [
+                .product(name: "EasyString", package: "easy-string"),
+                "SFSafeSymbols",
+            ]
+        ),
     ]
 )
--- Sources/EasyUtilities/EasyUtilities.swift
+++ Sources/EasyUtilities/EasyUtilities.swift
@@ -1,2 +1,44 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+@_exported import Foundation
+@_exported import UIKit
+@_exported import EasyString
+@_exported import SFSafeSymbols
+
+public extension UIImage {
+    func toData() -> Data? {
+        self.jpegData(compressionQuality: 1.0)
+    }
+}
+
+public extension Data {
+    func jpeg() -> UIImage? {
+        UIImage(data: self)
+    }
+}
+
+public extension CGPoint {
+    func distance(from other: CGPoint) -> CGFloat {
+        let first = (other.x - self.x) * (other.x - self.x)
+        let second = (other.y - self.y) * (other.y - self.y)
+        return pow(first + second, 0.5)
+    }
+}
+extension CGPoint: Identifiable {
+    public var id: String {
+        String(Float(self.x)) + String(Float(self.y))
+    }
+}
+
+public extension Float {
+    var cgFloat: CGFloat {
+        CGFloat(self)
+    }
+}
+
+public extension CGFloat {
+    var float: Float {
+        Float(self)
+    }
+}
