8129b2d6cc32afc860532e2a4e1a5105929d9cb8
Yuki Kuwashima <yukiny0811@gmail.com>
2023-12-03 10:24:47 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/yukiny0811/easy-string", from: "1.0.0"),
         .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", from: "4.1.1"),
-        .package(url: "https://github.com/sindresorhus/Defaults", from: "7.3.1"),
+        .package(url: "https://github.com/sindresorhus/Defaults", from: "7.0.0"),
         .package(url: "https://github.com/omaralbeik/Drops", from: "1.7.0"),
         .package(url: "https://github.com/malcommac/SwiftDate", from: "7.0.0"),
     ],
